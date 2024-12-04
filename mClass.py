30606caa87e811b7565024039f1c46cdfa3a425c
Joancc04 <72405888+Joancc04@users.noreply.github.com>
2024-12-05 00:37:07 +0100

    gaussian mixture model en procés

--- mClass.py
+++ mClass.py
@@ -1,5 +1,9 @@
 import os
 import pandas as pd
+from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.decomposition import PCA
 
 class MentalhealthML():
     def __init__(self, datafile=None):
@@ -26,9 +30,45 @@ class MentalhealthML():
         self._df = pd.read_csv(file)
         print(self._df.head(10))
 
-    def cleanse(self):
-        ...
+    def cleanse(self, *args):
+         for column in args:
+            if column in self._df.columns:
+                self._df.drop(column, axis=1, inplace=True)
+            else:
+                print(f"Warning: Column '{column}' not found in DataFrame")
 
+    
+    def gmm(self, n_clusters=3): # gaussian mixture model
+        numeric_df = self._df.select_dtypes(include=['float64', 'int64'])
+        # print(numeric_df.head(10))
+        gmm = GaussianMixture(n_components=n_clusters, covariance_type='full').fit(numeric_df)
+        labels = gmm.predict(numeric_df)
+        self._df['GMM_Cluster'] = labels
+        print("LABELS:", labels)
+        print(f"GMM clustering completed with {n_clusters} components.")
 
-# a = MentalhealthML()
-# a.load_dataset("datassdfet.csv")
+        # plot
+        pca = PCA(n_components=2)
+        components = pca.fit_transform(numeric_df)
+        plt.figure(figsize=(10, 8))
+        sns.scatterplot(x=components[:, 0], y=components[:, 1], hue=self._df['GMM_Cluster'], palette='Set2')
+        plt.title('GMM Clusters (PCA Reduced to 2D)')
+        plt.xlabel('Principal Component 1')
+        plt.ylabel('Principal Component 2')
+        plt.legend(title="Cluster")
+        plt.show()
+
+        columnes_clau = ['job', 'stud_h', 'cesd', 'stai_t', 'qcae_cog', 'amsp', 'health', 'mbi_ex']  # podem escollir qualsevol paràmetre per analitzar
+        plt.figure(figsize=(15, 10))
+
+        for i, feature in enumerate(columnes_clau, 1):
+            plt.subplot(3, 3, i)  
+            sns.boxplot(x='GMM_Cluster', y=feature, data=self._df, palette='Set2')  
+            plt.title(f'Distribució de {feature} per cluster')
+
+        plt.tight_layout()
+        plt.show()
+
+a = MentalhealthML()
+a.load_dataset("dataset.csv")
+print(a.gmm())
